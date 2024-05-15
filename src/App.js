eef82f66e13064246a0b76bdd5e1ad704f94f6cb
Andres Järviste <andres.jarviste@wbscodingschool.com>
2024-05-15 19:32:34 +0300

    loeng01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './luuletus.css'
+import suusataja from './suusataja1.jpg'
+import Luuletus from './Luuletus';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1>Maailma parim luuletus</h1>
+        <Luuletus />
+        <img className='img-responsive'  src={suusataja} alt='suusataja' />
     </div>
   );
 }
---
+++ src/Luuletus.js
@@ -0,0 +1,17 @@
+function Luuletus() {
+    return (
+        <div className="luuletus">
+            <p>
+                Kes hommikuti külas käib, see asjatult ei longi
+            </p>
+            <p>
+                param-param-param-paraa
+            </p>
+            <p>
+                sest selleks hommik ongi
+            </p>
+        </div>
+    )
+}
+
+export default Luuletus
